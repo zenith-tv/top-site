0cdf400c9342f434e468b81547bb42eeff55e015
Yann LANCELIN <yann@zenith-tv.fr>
2025-09-22 15:10:37 +0000

    maintenant le fond est invisible..

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,9 +67,9 @@ export default {
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: '0',
+        md: '0',
+        sm: '0',
       },
       keyframes: {
         'accordion-down': {
