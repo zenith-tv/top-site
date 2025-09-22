e0cf2edeea80444a11a1825c2b1dec674392cd00
Yann LANCELIN <yann@zenith-tv.fr>
2025-09-22 15:33:44 +0000

    fixer taille et position logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default async function Home() {
     <div className="h-screen w-screen overflow-y-auto">
       <header className="py-10">
         <div className="container mx-auto text-center px-4">
-          <div className="inline-flex items-center justify-center">
-            <Logo className="w-auto h-24 sm:h-28 md:h-32 text-primary" />
+          <div className="flex items-center justify-center">
+            <Logo className="w-auto h-20 text-primary" />
           </div>
           <p className="mt-4 max-w-2xl mx-auto text-lg text-muted-foreground">
             votez pour vos chansons préférées et faites-les monter dans le classement!
